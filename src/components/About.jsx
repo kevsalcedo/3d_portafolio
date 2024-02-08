6312d67d44dc98a333365d64b62daf28c1d662b8
kevsalcedo <kevsalcedom@gmail.com>
2024-02-08 14:23:33 -0500

    First commit

---
+++ src/components/About.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img 
+            src={icon} 
+            alt="title" 
+            className="w-16 h-16 object-contain" 
+          />
+
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Full Stack and mobile application developer with B2 English proficiency.
+        Experience in development and maintenance of Android applications using
+        Kotlin and Flutter language. Development of projects using JavaScript,
+        Next.js, Node.js, PostgreSQL, Express among other technologies. With
+        mastery of the MVVM architecture and the use of different tools and
+        libraries. I have experience with agile methodologies (SCRUM) and clean
+        architecture. <br /> <br /> I am very interested in the development of
+        mobile and multiplatform applications. I am a person who likes to be in
+        constant growth; learning and contributing my knowledge to the
+        development team in which I am. I consider myself a responsible, active
+        person, with leadership qualities and ability to be an excellent
+        partner. I value ethics, empathy, order and scalability in my work
+        environments.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
